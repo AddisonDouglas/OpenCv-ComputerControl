a18907e2d83d7210f76503cea81ad0e83a358aca
Addison Douglas <addisondouglas850@gmail.com>
2024-03-06 10:42:33 -0800

    :

--- Main.py
+++ Main.py
@@ -22,7 +22,7 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            for id, lm in enumerate(handLms.landmark)
+            for id, lm in enumerate(handLms.landmark):
                 print(id,lm)
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
 
